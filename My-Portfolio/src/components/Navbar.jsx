9ff4c47da7c8f386a8b078f11526e9defaa1c9e3
MadhuPojha <mpriya1201@gmail.com>
2025-05-18 15:22:56 +0200

    Update deployment configuration

--- My-Portfolio/src/components/Navbar.jsx
+++ My-Portfolio/src/components/Navbar.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import styles from "../CSS/Navbar.module.css";
 import { getImageUrl } from "../utils";
+import menuIcon from '../assets/menuIcon.png';
 
 export default function Navbar(){
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="">
-        Portfolio
-      </a>
+
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
+          src={menuIcon}
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
         />
@@ -25,6 +20,11 @@ export default function Navbar(){
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
         >
+          <li> 
+            <a className={styles.title} href="">
+              Home
+            </a>    
+          </li>
           <li>
             <a href="#about">About</a>
           </li>
